50f1f9e2e8dd6d974e8251d1bc48c38fda2caa8a
Do Duc Toan <112148624+DoDucToan2811@users.noreply.github.com>
2024-03-18 15:07:42 +0700

    Add files via upload

---
+++ software_timer.h
@@ -0,0 +1,18 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Mar 18, 2024
+ *      Author: doduc
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+void setTimer(int index, int value);
+int isTimerExpired(int index);
+
+void TimerRun();
+
+
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
